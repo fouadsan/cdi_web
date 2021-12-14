e443370a65cac6ce729d353e574afa2b74856192
Fouad B <74966625+fouadsan@users.noreply.github.com>
2021-12-14 14:06:25 +0100

    Update settings.py

--- cdi/cdi/settings.py
+++ cdi/cdi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n_b9j0igx4+m@qv%h6xmq7(067=0_uowskfz=_(v57&l(x-!lu'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
